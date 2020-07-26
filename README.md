76cdb1821f3710aea0c427dab3bfe9bf472f50bd
ramishenouda <ramishenoouda@outlook.com>
2020-07-26 13:05:55 +0200

    react revision

--- README.md
+++
@@ -1,3 +0,0 @@
-# React_TodoList
-
-### Simple TodoList using react
---
+++ precache-manifest.95c8eefc9f1bc21673a4b7d4b1895eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee53ed9f0201f35a5d13e59e3774754c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "730badefa4aba694f1ea",
+    "url": "./static/css/main.499b1b72.chunk.css"
+  },
+  {
+    "revision": "f67da48931c512b5e899",
+    "url": "./static/js/2.60e3458b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.60e3458b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730badefa4aba694f1ea",
+    "url": "./static/js/main.2d2be14c.chunk.js"
+  },
+  {
+    "revision": "f2886ae4f32e95d0a917",
+    "url": "./static/js/runtime-main.b4c0ae84.js"
+  }
+]);
